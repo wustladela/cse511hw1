5936a21ca1cfd6e55e7a3157af9427e07736ab21
Adela Gao <adelagao@adelas-mbp.dhcp.wustl.edu>
2015-09-12 17:59:06 -0500

    prepare to add while loop for UCS

--- search.py
+++ search.py
@@ -87,22 +87,37 @@ def depthFirstSearch(problem):
 
     from sets import Set
     from game import Directions
+
     fringe = util.Stack()
     start = problem.getStartState()
     explored = Set([start])
     path = []
+
     if problem.isGoalState(start):
         return path
+
+        """
+        get successors:
+          state: Search state
+
+        For a given state, this should return a list of triples,
+        (successor, action, stepCost), where 'successor' is a
+        successor to the current state, 'action' is the action
+        required to get there, and 'stepCost' is the incremental
+        cost of expanding to that successor
+        """
     startSucc = problem.getSuccessors(start)
     for ssucc in startSucc:
         node = (ssucc[0], ssucc[1])
+        print "node is: %s" %(node,)
         fringe.push(node)
     while not fringe.isEmpty():
-        if not fringe:
+        if not fringe:#???
             return []
         leaf = fringe.pop()
+        #print "popped leaf is: %s"%(leaf,)
         path = []
-        if type(leaf[1]) == str:
+        if type(leaf[1]) == str:#if there is only one node.
             path = [leaf[1],]
         else: 
             path = leaf[1]
@@ -111,6 +126,7 @@ def depthFirstSearch(problem):
             return path
         else:
             possMoves = problem.getSuccessors(leaf[0])
+            #print "possible moves:"
             for succ in possMoves:
                 if succ[0] not in explored:
                     tempPath = list(path)
@@ -127,11 +143,76 @@ def breadthFirstSearch(problem):
     [2nd Edition: p 73, 3rd Edition: p 82]
     """
     "*** YOUR CODE HERE ***"
+    from sets import Set
+    from game import Directions
+    fringe = util.Queue()
+    start = problem.getStartState()
+    explored = Set([start])
+    path = []
+    if problem.isGoalState(start):
+        return path
+    startSucc = problem.getSuccessors(start)
+    for ssucc in startSucc:
+        node = (ssucc[0], ssucc[1])
+        fringe.push(node)
+    while not fringe.isEmpty():
+        leaf = fringe.pop()
+        path = []
+        if type(leaf[1]) == str:
+            path = [leaf[1],]
+        else: 
+            path = leaf[1]
+        explored.add(leaf[0])
+        if problem.isGoalState(leaf[0]):
+            return path
+        else:
+            possMoves = problem.getSuccessors(leaf[0])
+            for succ in possMoves:
+                if succ[0] not in explored and succ[0] not in fringe.list:
+                    tempPath = list(path)
+                    tempPath.append(succ[1])
+                    node = (succ[0],tempPath)
+                    fringe.push(node)
     util.raiseNotDefined()
 
 def uniformCostSearch(problem):
     "Search the node of least total cost first. "
     "*** YOUR CODE HERE ***"
+    from sets import Set
+    from game import Directions
+
+    fringe = util.Stack()
+    start = problem.getStartState()
+    explored = Set([start])
+    path = []
+
+    if problem.isGoalState(start):
+        return path
+    allSuccessors = problem.getSuccessors(start)
+
+
+    
+    
+    for eachS in allSuccessors:
+        if problem.isGoalState(eachS[0]):
+            path.append(eachS[1])
+            return path
+        print "each successor, action, stepCost: %s"%(eachS,)
+        print "each stepcost: %s"%(eachS[2])
+
+    minNode = min(allSuccessors, key = lambda k: k[2])
+    print "mininum cost Node: %s"%(minNode,)
+
+    #expand the cheapest node and keep going
+    # fringe.push(minNode)
+    # while not fringe.isEmpty():
+    #     leafNode = fringe.pop()
+    #     path = []
+    #     if type(leafNode[1]) == str:
+    #         path = [leaf[1],]
+    #     else:
+
+
     util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
